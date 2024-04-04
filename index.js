035584e1deac30de6b25d14840106cba696cbd40
Candice Thomason <candice.thomason@gmail.com>
2024-04-04 17:51:36 +1100

    started lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your solution in this file!
+const employee = {
+  name: "John",
+  surname: "smith",
+};
+
+function updateEmployeeWithKeyAndValue(employee) {
+  employee.name = "Sam";
+}
+console.log(updateEmployeeWithKeyAndValue());
