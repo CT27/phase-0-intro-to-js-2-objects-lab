ef5a0e7364b1fc6cb58b66d12d0e0bc7d8b64ecd
Candice Thomason <candice.thomason@gmail.com>
2024-04-05 18:52:36 +1100

    4 out of 7 pass

--- index.js
+++ index.js
@@ -5,10 +5,21 @@ const employee = {
   streetAddress: "10 Loop street",
 };
 
-console.log(employeeWithKeyAndValue());
+console.log(employee);
 
 //2.
-function updateEmployeeWithKeyAndValue(employee, key, value) {
-  return { name: "Sam", ...1, streetAddress: "11 Broadway", ...2 };
+function updateEmployeeWithKeyAndValue() {
+  return { name: "Sam", ...1, streetAddress: "11 Broadway" };
 }
-console.log(updateEmployeeWithKeyAndValue());
+
+//3.
+function destructivelyUpdateEmployeeWithKeyAndValue() {
+  employee.name = "Sam";
+  employee.streetAddress = "12 Broadway";
+  return employee;
+}
+
+//4.
+function deleteFromEmployeeByKey() {
+  const newEmployee = { ...employee };
+}
